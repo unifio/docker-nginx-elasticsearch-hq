332d0c0681decc306d3d2f1850ef9eadebf511b1
Daniel <isaac@unif.io>
2017-09-29 16:59:24 -0700

    Allowing management of loadbalanced ELK cluster
    - Removed ssl verify since proxying request to SSL load balancer.
    - Tested and works fine proxying request to loadbalancer.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 ES_PORT=${ES_PORT:-9200}
 ES_HOST=${ES_HOST:-elasticsearch}
-
+ES_SCHEME=${ES_SCHEME:-https}
 CONFDIR=/etc/nginx/conf
 [ -d /etc/nginx/conf.d ] && CONFDIR=/etc/nginx/conf.d
 
@@ -21,25 +21,26 @@ server {
   error_log             /dev/stderr;
 
   proxy_read_timeout 90;
-  proxy_http_version 1.1;
+  # proxy_http_version 1.1;
   proxy_set_header Connection "Keep-Alive";
   proxy_set_header Proxy-Connection "Keep-Alive";
 
-  proxy_set_header X-Real-IP \$proxy_add_x_forwarded_for;
-  proxy_set_header X-ELB-IP \$remote_addr;
-  proxy_set_header X-ELB-Forwarded-For \$proxy_add_x_forwarded_for;
-  proxy_set_header Host \$http_host;
-  proxy_set_header Referer \$http_referer;
+  # proxy_set_header X-Real-IP \$proxy_add_x_forwarded_for;
+  # proxy_set_header X-ELB-IP \$remote_addr;
+  # proxy_set_header X-ELB-Forwarded-For \$proxy_add_x_forwarded_for;
+  # proxy_set_header Host \$http_host;
+  # proxy_set_header Referer \$http_referer;
 
-  # For CORS Ajax
-  proxy_pass_header Access-Control-Allow-Origin;
-  proxy_pass_header Access-Control-Allow-Methods;
-  proxy_hide_header Access-Control-Allow-Headers;
-  add_header Access-Control-Allow-Headers 'X-Requested-With, Content-Type';
-  add_header Access-Control-Allow-Credentials true;
+  # # For CORS Ajax
+  # proxy_pass_header Access-Control-Allow-Origin;
+  # proxy_pass_header Access-Control-Allow-Methods;
+  # proxy_hide_header Access-Control-Allow-Headers;
+  # add_header Access-Control-Allow-Headers 'X-Requested-With, Content-Type';
+  # add_header Access-Control-Allow-Credentials true;
 
   location /elastic-search-proxy {
-    proxy_pass http://elasticsearch/;
+    proxy_ssl_verify              off;
+    proxy_pass ${ES_SCHEME}://elasticsearch;
     expires max;
     access_log on;
   }
